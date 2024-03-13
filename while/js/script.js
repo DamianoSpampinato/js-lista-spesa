7958bc63327e9ce0f53327f2f5a1125fe0ddcc24
Damiano <ghgg585@gmail.com>
2024-03-13 18:20:28 +0100

    feat add while cycle

--- while/js/script.js
+++ while/js/script.js
@@ -7,4 +7,10 @@ const list = [
     'Piatti pronti'
 ];
 let thisContainer = document.querySelector('#container'); 
-
+let i = 0;
+while(i < list.length){
+    const listItemDiv = document.createElement('div');
+    listItemDiv.innerHTML = list[i];
+    thisContainer.append(listItemDiv);
+    i++;
+}
