6bb7cb94180597130358bed772d7c8564ee0ed45
Damiano <ghgg585@gmail.com>
2024-03-13 18:14:09 +0100

    Feat:add for cycle

--- for/js/script.js
+++ for/js/script.js
@@ -6,3 +6,10 @@ const list = [
     'Pomodori',
     'Piatti pronti'
 ];
+let thisContainer = document.querySelector('#container'); 
+
+for(let i = 0; i < list.length; i++){
+const listItemDiv = document.createElement('div');
+listItemDiv.innerHTML = list[i];
+thisContainer.append(listItemDiv);
+}
